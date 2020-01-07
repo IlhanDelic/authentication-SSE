a08e8736c735a922463763c1b9c1836c9e2c1193
0914619 <0914619@hr.nl>
2020-01-07 16:18:04 +0100

    local db

--- Acentral-event-server/src/server.js
+++ Acentral-event-server/src/server.js
@@ -11,116 +11,22 @@ var express = require("express");
 var app = express();
 app.set('view-engine', 'ejs');
 var readline = require('readline');
-var users = []; // empty array to store the users for this test program
+var users = [{ id: '1', email: 'bob@mail.com', password: 'bob', name: 'bob', location: 'roomA' },
+    { id: '2', email: 'jay@mail.com', password: 'jay', name: 'jay', location: 'roomB' },
+    { id: '3', email: 'rik@mail.com', password: 'rik', name: 'rik', location: 'roomC' },
+    { id: '4', email: 'piet@mail.com', password: 'piet', name: 'piet', location: 'classified' }
+]; // array to store the users for this test program
 //----------------------------------------------------------------------------------makes sure you can work with input
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 //---------------------------------------------------------------------------------------------------------------
-app.get('/', function (req, res) {
-    main();
-    res.send('home');
-    console.log('home page');
-});
-app.get('/countdown', function (req, res) {
-    res.set({
-        connection: "keep alive",
-        "cache-control": "no-cache",
-        "content-Type": "text/event-stream",
-    });
-    countdown(res, 13);
-});
-app.get('/register', function (req, res) {
-    res.send('register');
-    console.log('register page');
-});
-app.get('/login', function (req, res) {
-    res.send('login');
-    console.log('login page');
-});
-main();
-// ---------------------------------------------------------------------------------makes sure you can work with input
-function main() {
-    rl.question("do you want to register as a new user [r] \n" +
-        "do you want to log in [l] \n" + "See counter [c] \n\n", function (answer) {
-        switch (answer.toLowerCase()) {
-            case 'r':
-                console.log("you need to register a new account.");
-                //TODO redirect instead of app.get
-                register();
-                break;
-            case 'l':
-                console.log("you need too  log in.");
-                login();
-                break;
-            case 'stop':
-                rl.question("do you want to quit? [y/n]", function (quit) {
-                    switch (quit.toLocaleLowerCase()) {
-                        case 'y':
-                            return;
-                        case 'n':
-                            main();
-                            break;
-                        default:
-                            return;
-                    }
-                });
-                break;
-            case 'c':
-                app.get('/countdown', function (req, res) {
-                    res.set({
-                        connection: "keep alive",
-                        "cache-control": "no-cache",
-                        "content-Type": "text/event-stream",
-                    });
-                    countdown(res, 30);
-                });
-                break;
-            default:
-                console.log("you need to choose something");
-                main();
-        }
-        main();
-    });
-}
-//---------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------ function for registering a user
-function register() {
-    rl.question('enter your email: ', function (email) {
-        rl.question('enter your username: ', function (username) {
-            rl.question('enter your password: ', function (password) {
-                users.push({
-                    id: Date.now().toString(),
-                    email: email,
-                    username: username,
-                    password: password
-                });
-                console.log(users);
-                main();
-            });
-        });
-    });
-}
-//---------------------------------------------------------------------------------------------------------------
-function login() {
-    rl.question("enter email: ", function (username) {
-        rl.question("enter password: ", function (password) {
-            //TODO authentication, id/token/userinfo
-        });
-    });
-}
-//---------------------------------------------------------------------------------------------------------------
-function countdown(res, count) {
-    var id = 0;
-    res.write("data: time left " + count + " \n\n");
-    if (count) {
-        setTimeout(function () { return countdown(res, count - 1); }, 1000);
-        id++;
+rl.question('enter your email: ', function (userMail) {
+    function findEmail(x) {
+        return x.email === userMail;
     }
-    else {
-        res.write("times up!\n\n\n");
-        res.end();
-    }
-}
-app.listen(3000, function () { return console.log('go to localhost:3000'); });
+    console.log(users.find(findEmail));
+});
+//let data = users.filter(t=>t.email===x);
+//console.log(data);
--- Acentral-event-server/src/server.ts
+++ Acentral-event-server/src/server.ts
@@ -11,7 +11,13 @@ import express = require("express");
 const app = express();
 app.set('view-engine', 'ejs');
 const readline = require('readline');
-const users:any = []; // empty array to store the users for this test program
+
+const users = [ {id:'1', email:'bob@mail.com', password:'bob', name:'bob', location:'roomA'},
+                {id:'2', email:'jay@mail.com', password:'jay', name:'jay', location:'roomB'},
+                {id:'3', email:'rik@mail.com', password:'rik', name:'rik', location:'roomC'},
+                {id:'4', email:'piet@mail.com', password:'piet', name:'piet', location:'classified'}
+                ]; // array to store the users for this test program
+
 //----------------------------------------------------------------------------------makes sure you can work with input
 const rl = readline.createInterface({ //rl reads the input from the command lines
     input: process.stdin,
@@ -19,127 +25,23 @@ const rl = readline.createInterface({ //rl reads the input from the command line
 });
 //---------------------------------------------------------------------------------------------------------------
 
-
-
-app.get('/', (req, res) => {  // really important to make sure to put req first and res second because it wont work if res is first
-    main();
-    res.send('home');
-    console.log('home page');
-});
-
-app.get('/countdown', (req, res) =>{
-
-    res.set({
-        connection: "keep alive",
-        "cache-control": "no-cache",
-        "content-Type": "text/event-stream",
-    });
-    countdown(res, 13)
-});
-
-app.get('/register', (req, res) => {
-
-   res.send('register');
-   console.log('register page');
-});
-
-app.get('/login', (req, res) => {
-   res.send('login');
-   console.log('login page');
-});
-
-main();
-
-// ---------------------------------------------------------------------------------makes sure you can work with input
-function main() {
-    rl.question(`do you want to register as a new user [r] \n` +
-        `do you want to log in [l] \n` + `See counter [c] \n\n`, (answer:string) => {
-        switch (answer.toLowerCase()) {
-            case 'r':
-                console.log("you need to register a new account.");
-                //TODO redirect instead of app.get
-                register();
-                break;
-
-            case 'l':
-                console.log("you need too  log in.");
-                    login();
-                break;
-
-            case 'stop':
-                rl.question("do you want to quit? [y/n]", (quit: string) =>{
-                switch (quit.toLocaleLowerCase()) {
-                    case 'y':
-                        return;
-
-                    case 'n':
-                        main();
-                        break;
-
-                    default:
-                        return;  } });
-            break;
-
-            case 'c':
-                app.get('/countdown', (req, res) =>{
-
-                    res.set({
-                        connection: "keep alive",
-                        "cache-control": "no-cache",
-                        "content-Type": "text/event-stream",
-                    });
-                    countdown(res, 30)
-                });
-                break;
-
-            default:
-                console.log("you need to choose something");
-                    main();
-        }
-        main()
-    });
-}
-//---------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------ function for registering a user
-
-function register(){
-
-    rl.question('enter your email: ', (email:string) => {
-        rl.question('enter your username: ', (username:string) => {
-            rl.question('enter your password: ', (password:string) => {
-                users.push({
-                    id: Date.now().toString(),
-                    email: email,
-                    username: username,
-                    password: password
-                });
-                console.log(users);
-                main();
-            });
-        });
-    });
-}
-//---------------------------------------------------------------------------------------------------------------
- function login() {
-    rl.question("enter email: ", (username:string) =>{
-        rl.question("enter password: ", (password:string) => {
-            //TODO authentication, id/token/userinfo
-        });
-    });
-}
-//---------------------------------------------------------------------------------------------------------------
-function countdown(res: any, count: number){
-    let id = 0;
-    res.write(`data: time left ${count} \n\n`);
-    if (count){
-        setTimeout(() => countdown(res, count-1), 1000);
-        id++;
+rl.question('enter your email: ', (userMail:string) => {
+    function findEmail (x:any) {
+        return x.email === userMail;
     }
-    else {
-        res.write(`times up!\n\n\n`);
-        res.end();
-    }
-}
+        rl.question('enter your password', (password:string) => {
+            function findPw (x:any) {
+                return x.email === userMail;
 
-app.listen(3000, () => console.log ('go to localhost:3000'));
+            }
+            console.log(users.find(findEmail));
+            console.log(users.map(findEmail));
+        })
+});
+
+app.get('/form', (req, res)=>{
+
+});
+
+
 
